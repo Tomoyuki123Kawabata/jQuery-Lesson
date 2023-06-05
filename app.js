e985892d148ff9335dcf2d29db037ea9dcf49fc5
Tomoyuki123Kawabata <122082755+Tomoyuki123Kawabata@users.noreply.github.com>
2023-06-05 14:22:28 +0000

    term5

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-$(document).ready(function () {
-  // jQueryプログラムの内容
+$(function (){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
 });
